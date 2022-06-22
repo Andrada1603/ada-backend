a368f90c8699bb8c008b07a6628d205b4f5b86c4
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2022-06-22 16:17:44 +0300

    Authorize returns true for postman purposes

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,3 +1,4 @@
+// TODO: uncomment in production
 const jwt = require('jsonwebtoken');
 
 /**
@@ -5,34 +6,37 @@ const jwt = require('jsonwebtoken');
  * @see https://www.npmjs.com/package/express-jwt
  */
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization) {
-    return res.status(401).json({
-      name: 'Error',
-      message: 'Unauthorized',
-    });
-  }
+  // const { authorization } = req.headers;
+  req.user = {}
+  next();
 
-  const token = authorization.split(' ').reverse()[0];
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({
-        name: 'Error',
-        message: `Unauthorized ${err.name}`,
-      });
-    }
+  // if (!authorization) {
+  //   return res.status(401).json({
+  //     name: 'Error',
+  //     message: 'Unauthorized',
+  //   });
+  // }
 
-    const { me, email } = decoded;
-    if (!me || !email) {
-      return res.status(401).json({
-        name: 'Error',
-        message: 'Unauthorized Malformed JWT',
-      });
-    }
+  // const token = authorization.split(' ').reverse()[0];
+  // jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+  //   if (err) {
+  //     return res.status(401).json({
+  //       name: 'Error',
+  //       message: `Unauthorized ${err.name}`,
+  //     });
+  //   }
 
-    // Make the decoded JWT payload available on the request object
-    req.user = decoded;
+  //   const { me, email } = decoded;
+  //   if (!me || !email) {
+  //     return res.status(401).json({
+  //       name: 'Error',
+  //       message: 'Unauthorized Malformed JWT',
+  //     });
+  //   }
 
-    next();
-  });
+  //   // Make the decoded JWT payload available on the request object
+  //   req.user = decoded;
+
+  //   next();
+  // });
 };
