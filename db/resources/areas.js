f5a09c9aaaada43b01987821a8161830481c1e92
ANDRADA POPA <POPAANDRADA19@stud.ase.ro>
2022-06-23 14:24:39 +0300

    Update "Adresse" to "adress"

--- db/resources/areas.js
+++ db/resources/areas.js
@@ -3,32 +3,32 @@ module.exports = async () => {
     {
       name: 'Sala Ion Mihalache',
       dimension: 1000,
-      adresse: 'Bdul. Ion Mihalache 48'
+      adress: 'Bdul. Ion Mihalache 48'
     },
     {
       name: 'Sala Tei',
       dimension: 1500,
-      adresse: 'Str. Tei 25'
+      adress: 'Str. Tei 25'
     },
     {
       name: 'Sala Virgil Madgearu',
       dimension: 200,
-      adresse: 'Bdul. Virgil Madgearu 1'
+      adress: 'Bdul. Virgil Madgearu 1'
     },
     {
       name: 'Sala WorldClass',
       dimension: 600,
-      adresse: 'Bdul. Unirii 44A'
+      adress: 'Bdul. Unirii 44A'
     },
     {
       name: 'Pantelimon',
       dimension: 1500,
-      adresse: 'Bdul. Pantelimon 2'
+      adress: 'Bdul. Pantelimon 2'
     },
     {
       name: 'Arcca',
       dimension: 500,
-      adresse: 'Str. Vitan 23'
+      adress: 'Str. Vitan 23'
     },
   ];
 };
--- models/area.js
+++ models/area.js
@@ -15,7 +15,7 @@ const schema = new mongoose.Schema({
     type: Number,
     required: true,
   },
-  adresse: {
+  adress: {
     type: String,
     required: true,
   },
