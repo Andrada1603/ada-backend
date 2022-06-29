29ba70ef621667246313e870bb55b971d902d2f4
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2022-06-29 11:38:40 +0300

    Adds lessons and sports to coach read one

--- controllers/coach/read-one.js
+++ controllers/coach/read-one.js
@@ -1,5 +1,5 @@
 const { error } = require('../../functions');
-const { Coach } = require('../../models');
+const { Coach, Lesson, Sport } = require('../../models');
 
 module.exports = async (req, res) => {
   const { id } = req.params;
@@ -8,10 +8,18 @@ module.exports = async (req, res) => {
     throw error(404, 'Missing required params');
   }
 
-  const coach = await Coach.findById(id);
+  const coach = await Coach.findById(id).lean().exec();
   if (!coach) {
     throw error(404, 'Resource not found');
   }
+
+  // adding lessons
+  const lessons = await Lesson.find({ coach: id });
+  coach.lessons = lessons;
+
+  // adding sports
+  const sports = await Sport.find({ coaches: { $in: [id] } });
+  coach.sports = sports;
 
   return res.status(200).json(coach);
 };
