89cdccafca92a53253cbd49f95b795aab8e74c92
ANDRADA POPA <POPAANDRADA19@stud.ase.ro>
2022-07-15 14:07:42 +0300

    update seed for identities email

--- db/resources/identities.js
+++ db/resources/identities.js
@@ -3,28 +3,12 @@ const { hashSync } = require('bcryptjs');
 module.exports = async () => {
   return [
     {
-      email: 'michael@email.com',
-      name: 'Michael Scott',
+      email: 'admin@email.com',
+      name: 'Andrada Popa',
       role: 'admin',
       password: hashSync('supersecretpassword'),
       active: true,
       confirmed: true,
-    },
-    {
-      email: 'jim@email.com',
-      name: 'Jim Halpert',
-      role: 'client',
-      password: hashSync('supersecretpassword'),
-      active: false,
-      confirmed: true,
-    },
-    {
-      email: 'pam@email.com',
-      name: 'Pam Beesly',
-      role: 'client',
-      password: hashSync('supersecretpassword'),
-      active: true,
-      confirmed: false,
     },
   ];
 };
